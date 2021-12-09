b63428c347d8a705fff09cc27343ed69cbfe2bd5
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2021-12-09 22:19:44 +0900

    feat Add TypeScript/React

---
+++ dist/index.ts
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const log = (message) => {
+    console.log(message);
+};
+log('Hello, Webpack + TypeScript!');
+
+/******/ })()
+;
