f6726c8887173d7db13b25d2d7c7069488e24475
TETSU <tetsu.work.it@gmail.com>
2021-12-10 21:54:06 +0900

    Delete dist directory

--- dist/index.ts
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-const log = (message) => {
-    console.log(message);
-};
-log('Hello, Webpack + TypeScript!');
-
-/******/ })()
-;
