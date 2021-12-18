5d17aa301a7c0e3c8a2acc440487f5d39ec19976
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2021-12-18 19:51:41 +0900

    12-17

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -48,12 +48,14 @@ const Preview = styled.div`
   width: 50vw;
 `
 
-const StorageKey = 'pages/editor:text'
+interface Props {
+  text: string
+  setText: (text: string) => void
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage('',StorageKey)
-
-const [showModal, setShowModal] = useState(false)
+export const Editor: React.FC<Props> = (props) => {
+  const {text, setText} = props
+  const [showModal, setShowModal] = useState(false)
 
   return (
     <>
