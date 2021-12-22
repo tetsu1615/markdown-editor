6d64e120e00bbb80d35ebf0d293f3075470da235
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2021-12-22 10:16:27 +0900

    12-21

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { useStateWithStorage } from '../hooks/use_state_with_storage'
-import * as ReactMarkdown from 'react-markdown'
 import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
 import { SaveModal } from '../components/save_modal'
 import { Link } from 'react-router-dom'
 import { Header } from '../components/header'
+import ConvertMarkdownWorker from 'worker-loader!../worker/convert_markdown_worker'
 
-const { useState } = React
+const convertMarkdownWorker = new ConvertMarkdownWorker()
+const { useState, useEffect } = React
 
 const Wrapper = styled.header`
   bottom:0;
@@ -56,6 +57,17 @@ interface Props {
 export const Editor: React.FC<Props> = (props) => {
   const {text, setText} = props
   const [showModal, setShowModal] = useState(false)
+  const [html, setHtml] = useState('')
+
+  useEffect(() => {
+    convertMarkdownWorker.onmessage = (event) => {
+      setHtml(event.data.html)
+    }
+  }, [])
+
+  useEffect(() => {
+    convertMarkdownWorker.postMessage(text)
+  }, [text])
 
   return (
     <>
@@ -75,7 +87,7 @@ export const Editor: React.FC<Props> = (props) => {
           value={text}
         />
         <Preview>
-          <ReactMarkdown>{text}</ReactMarkdown>
+          <div dangerouslySetInnerHTML = {{__html: html}} />
         </Preview>
       </Wrapper>
       {showModal && (
