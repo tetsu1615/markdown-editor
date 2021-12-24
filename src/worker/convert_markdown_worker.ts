abd955b487aaa6e09cef3f8528b5a5457574b7fa
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2021-12-24 18:16:08 +0900

    12-24

--- src/worker/convert_markdown_worker.ts
+++ src/worker/convert_markdown_worker.ts
@@ -1,10 +1,11 @@
 import * as marked from "marked"
+import * as sanitizedHtml from 'sanitize-html'
 
 const worker: Worker = self as any
 
 worker.addEventListener('message', (event) => {
 
   const text = event.data
-  const html = marked(text)
+  const html = sanitizedHtml(marked(text), { allowedTags: [...sanitizedHtml.defaults.allowedTags, 'h1', 'h2']})
   worker.postMessage({ html })
 })
